6396446c48b1e68c31bac47aec3d2d64466c5bd2
JackLeong99 <jackleong875@gmail.com>
2023-03-29 18:30:16 +1100

    Finally remembering version management in week 6

---
+++ ex/MyDsClientCanary.java
@@ -0,0 +1,28 @@
+import java.net.*;
+import java.io.*;
+class MyDsClientCanary{
+public static void main(String args[])throws Exception{
+Socket s=new Socket("127.0.0.1",61420);
+BufferedReader din=new BufferedReader(new InputStreamReader(s.getInputStream()));
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+String str="",str2="";
+while(!str.equals("stop")){
+str=br.readLine();
+
+if(str.equals("atl")){
+System.out.println("Running ATL...");
+
+}
+else{
+dout.write((str+"\n").getBytes());
+dout.flush();
+str2=din.readLine();
+System.out.println("Server: "+str2);
+}
+}
+
+dout.close();
+s.close();
+}}
