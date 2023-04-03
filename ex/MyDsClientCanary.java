32b6312ffcbfe5e5b3a47d7a7afd49466778b76a
JackLeong99 <jackleong875@gmail.com>
2023-04-03 15:37:31 +1000

    passed handshake

--- ex/MyDsClientCanary.java
+++ ex/MyDsClientCanary.java
@@ -2,27 +2,53 @@ import java.net.*;
 import java.io.*;
 class MyDsClientCanary{
 public static void main(String args[])throws Exception{
-Socket s=new Socket("127.0.0.1",61420);
-BufferedReader din=new BufferedReader(new InputStreamReader(s.getInputStream()));
-DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+  Socket s=new Socket("127.0.0.1",61420);
+  BufferedReader din=new BufferedReader(new InputStreamReader(s.getInputStream()));
+  DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
-String str="",str2="";
-while(!str.equals("stop")){
-str=br.readLine();
+  String localIn="",sin="",sout="";
+  boolean init=false;
+  //temporary hardcoded handshake
+  dout.write(("HELO\n").getBytes());
+  dout.flush();
+  while(true){
+    sin=din.readLine();
+    if(sin.equals("OK")){
+      sin="";
+      break;
+    }
+    else{
+      continue;
+    }
+  }
+  System.out.println("passed HELO");
+  dout.write(("AUTH PIPI\n").getBytes());
+  dout.flush();
+  while(true){
+    sin=din.readLine();
+    if(sin.equals("OK")){
+      sin="";
+      break;
+    }
+    else{
+      continue;
+    }
+  }
+  System.out.println("passed AUTH");
+  while(!sin.equals("NONE")){
+    dout.write(("REDY\n").getBytes());
+    dout.flush();
+    
+  }
 
-if(str.equals("atl")){
-System.out.println("Running ATL...");
 
+  dout.write(("QUIT\n").getBytes());
+  while(!sin.equals("QUIT")){
+    localIn=br.readLine();
+    sin=din.readLine();
+  }
+  dout.close();
+  s.close();
 }
-else{
-dout.write((str+"\n").getBytes());
-dout.flush();
-str2=din.readLine();
-System.out.println("Server: "+str2);
 }
-}
-
-dout.close();
-s.close();
-}}
