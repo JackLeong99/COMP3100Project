f3d7f7706e9e8214d6b4cbd0ca26eef801665668
Awoo_ <jackleong875@gmail.com>
2023-05-27 21:30:30 +1000

    implemented basic algorithm to test refactor

--- stage-1/dsClient.java
+++ stage-1/dsClient.java
@@ -5,14 +5,17 @@ class dsClient {
     Socket s;
     BufferedReader din;
     DataOutputStream dout;
+
 public void main(String args[])throws Exception{
   s=new Socket("127.0.0.1",50000);
   din=new BufferedReader(new InputStreamReader(s.getInputStream()));
   dout=new DataOutputStream(s.getOutputStream());
   auth();
   while(run){
-
+    sendMessage("REDY");
+    handleMessage();
   }
+  handleQuit();
   dout.close();
   s.close();
 }
@@ -41,12 +44,19 @@ public void handleMessage(){
         switch(stringBufferSplit[0]){
             case "JCPL":
                 //TODO handle jcpl message
+                //do i even need to handle jcpl messages?...
                 break;
             case "JOBN":
                 //TODO handle shedule msg based on algorithm type
+                //probably use EJWT, LSTJ or other message to get estimated work time left 
+                //for each server to find best candidate for job taker
+                handleJobnSRTN(stringBufferSplit);
                 break;
             case "NONE":
                     run = false;
+                break;
+            default:
+                //TODO default handle if needed
                 break;
         }
     } catch (IOException e){
@@ -55,6 +65,7 @@ public void handleMessage(){
 }
 
 public void handleMessage(String check){
+    //TODO make this return a string of recieved message 
     try {
         String stringBuffer = din.readLine();
         if(!stringBuffer.startsWith(check)){
@@ -63,6 +74,50 @@ public void handleMessage(String check){
     }  catch(IOException e) {
         e.printStackTrace();
     }
+}
+
+public void handleJobnSRTN(String[] JOBN){   
+    try{
+    String stringBuffer = ""; 
+    sendMessage("GETS Avail "+JOBN[4]+" "+JOBN[5]+" "+JOBN[6]);
+    //get DATA response
+    stringBuffer = din.readLine();
+    int nRecs = Integer.parseInt(stringBuffer.split("\\s+")[1]);
+    sendMessage("OK");
+    //Handle server listings
+    String targetServerType = "";
+    String targetServerID = "";
+    for(int i=0; i < nRecs; i++){
+        stringBuffer = din.readLine();
+        targetServerType = stringBuffer.split("\\s+")[0];
+        targetServerID = stringBuffer.split("\\s+")[1];
+    }
+    sendMessage("OK");
+    handleMessage(".");
+    if(nRecs > 0){
+        schdJOBN(JOBN[2], targetServerType, targetServerID);
+    }
+    else{
+        sendMessage("GETS Capable "+JOBN[4]+" "+JOBN[5]+" "+JOBN[6]);
+        stringBuffer = din.readLine();
+        nRecs = Integer.parseInt(stringBuffer.split("\\s+")[1]);
+        sendMessage("OK");
+        for(int i=0; i < nRecs; i++){
+            stringBuffer = din.readLine();
+            targetServerType = stringBuffer.split("\\s+")[0];
+            targetServerID = stringBuffer.split("\\s+")[1];
+        }
+        sendMessage("OK");
+        handleMessage(".");
+        schdJOBN(JOBN[2], targetServerType, targetServerID);
+    }
+    } catch(IOException e){
+        e.printStackTrace();
+    }   
+}
+
+public void schdJOBN(String jobID, String serverType, String serverID){
+    sendMessage("SCHD "+jobID+" "+serverType+" "+serverID);
 }
 
 public void handleQuit(){
